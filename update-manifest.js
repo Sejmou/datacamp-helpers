23db3939492fc98c10d88c55dc0a9864f77e7d60
Sejmou <samo.kolter@gmail.com>
2022-05-17 15:27:00 +0200

    try to fix import problems

--- update-manifest.js
+++ update-manifest.js
@@ -1,14 +1,16 @@
 // this is a workaround node.js script that adds the copy-helper script (and all modules required by it) to "web_accessible_resources" of manifest.json
-
-const fs = require('fs');
-const glob = require('glob');
+import { readFileSync, writeFileSync } from 'fs';
+import glob from 'glob';
 
 // https://stackoverflow.com/a/41462807/13727176
 async function getDirectories(root) {
   return new Promise((resolve, reject) =>
     glob(root + '/**/*', (err, fileNames) => {
       if (err) reject();
-      else resolve(fileNames);
+      else {
+        const jsFileNames = fileNames.filter(n => n.endsWith('.js'));
+        resolve(jsFileNames);
+      }
     })
   );
 }
@@ -16,13 +18,13 @@ async function getDirectories(root) {
 async function main() {
   const manifestPath = 'manifest.json';
 
-  const manifest = JSON.parse(fs.readFileSync(manifestPath));
-  const copyHelperFiles = await getDirectories('copy-helper');
+  const manifest = JSON.parse(readFileSync(manifestPath));
+  const copyHelperFilePaths = await getDirectories('copy-helper');
 
-  manifest.web_accessible_resources = copyHelperFiles;
+  manifest.web_accessible_resources = copyHelperFilePaths;
 
   // reason for weird-looking call to stringify() is pretty-printing: https://stackoverflow.com/a/5670892/13727176
-  fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+  writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
 }
 
 main();
