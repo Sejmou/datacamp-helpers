f92393757d2f083d20a3c28030e734d5276e6f89
Sejmou <samo.kolter@gmail.com>
2022-05-18 18:27:16 +0200

    move more stuff into DOM utils

--- copy-helper/copy-helper.js
+++ copy-helper/copy-helper.js
@@ -4,7 +4,14 @@ import { overviewCrawler } from './page-crawlers/course-overview.js';
 import { videoPageCrawler } from './page-crawlers/video-page.js';
 import { videoIframeCrawler } from './page-crawlers/video-iframe.js';
 import { exerciseCrawler } from './page-crawlers/code-exercise/index.js';
-import { selectElements, selectSingleElement } from './util/dom.js';
+import {
+  addStyle,
+  createButton,
+  createSnackbar,
+  showSnackbar,
+  selectElements,
+  selectSingleElement,
+} from './util/dom.js';
 
 // config for all types of exercises
 const includeTaskAndSolutionHeadings = true; // whether fitting subheadings for differentiating between task and task solution should be added automatically when copying exercises
@@ -24,6 +31,7 @@ const codeExerciseConfig = {
   maxConsoleOutLineWidth: 90, // recommended: 90 -> should be exactly width of regular R Markdown code cells
   splitConsoleOutOnProducedResult: true, // whether a seperate code block should be created for each code statement that causes some console output; if false, all console output is put into the same code block; NOTE: currently only works if copyEditorCodeFromConsoleOut is also true
 };
+
 // TODO: remove this global const if/when refactoring the codebase
 const warningSnackbarId = 'copy-helper-warning-snackbar';
 
@@ -234,12 +242,6 @@ function getURLQueryParams() {
   });
 }
 
-function addStyle(CSSText) {
-  const style = document.createElement('style');
-  style.appendChild(document.createTextNode(CSSText));
-  document.querySelector('head').appendChild(style);
-}
-
 function addSlideImageViewFeatures() {
   const imgs = selectElements(
     '.slide-content img:not([class]), .slide-content img[class=""]'
@@ -446,87 +448,8 @@ function createCopyButton() {
   return btn;
 }
 
-function createButton(text, id = null, className = null) {
-  const btn = document.createElement('button');
-  if (id) btn.id = id;
-  if (className) btn.className = className;
-  btn.innerText = text;
-  btn.type = 'button';
-  return btn;
-}
-
-function createSnackbar(
-  id,
-  pos = { top: '50%', left: '50%' },
-  textColor = 'white',
-  animationDuration = 3
-) {
-  const posCss = createObjWithTruthyValuesForProps(
-    ['top', 'right', 'bottom', 'left'],
-    pos
-  );
-
-  addStyle(`
-  #${id} {
-    display: none;
-    background-color: #333;
-    color: ${textColor};
-    text-align: center;
-    border-radius: 2px;
-    padding: 16px;
-    position: fixed;
-    z-index: 9999;
-    ${objToCssPropsAndValsStr(posCss)}
-    transform: translate(-50%, -50%);
-  }
-  
-  #${id}.visible {
-    /* https://stackoverflow.com/a/49546937/13727176 */
-    animation-fill-mode: forwards;
-    animation-name: fade-in, fade-out;
-    animation-delay: 0s, ${animationDuration - 0.25}s;
-    animation-duration: 0.25s; /* same for both */
-    display: flex;
-  }
-  
-  @keyframes fade-in {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 0.9;
-    }
-  }
-
-  @keyframes fade-out {
-    to {
-      opacity: 0;
-    }
-  }
-
-  `);
-
-  const snackbar = document.createElement('div');
-  snackbar.id = id;
-
-  return snackbar;
-}
-
 export function showWarning(msg) {
   showSnackbar(warningSnackbarId, `Warning: ${msg}`);
-}
-
-function showSnackbar(id, text) {
-  const snackbar = document?.getElementById(id);
-  snackbar.remove();
-  document.body.appendChild(snackbar);
-
-  if (snackbar) {
-    snackbar.innerText = text;
-    snackbar.classList.add('visible');
-  } else {
-    console.warn('Snackbar with ID', id, 'not found!');
-  }
 }
 
 function createConsoleOutputToggleCheckbox(checkboxId) {
@@ -572,21 +495,4 @@ function createConsoleOutputToggleCheckbox(checkboxId) {
   `);
 
   return container;
-}
-
-// creates new object from inputObj with only those props that are in the given array of props and have a truthy value in inputObj
-function createObjWithTruthyValuesForProps(props, inputObj) {
-  return props.reduce((prev, propName) => {
-    const propVal = inputObj[propName];
-    if (propVal) {
-      return { ...prev, [propName]: propVal };
-    }
-    return prev;
-  }, {});
-}
-
-function objToCssPropsAndValsStr(obj) {
-  return Object.entries(obj)
-    .map(([prop, val]) => `${prop}: ${val};`)
-    .join('\n');
 }
--- copy-helper/util/dom.js
+++ copy-helper/util/dom.js
@@ -1,5 +1,88 @@
 import { noLeadingWhitespace } from './strings.js';
 
+// DOM element creation
+
+export function createButton(text, id = null, className = null) {
+  const btn = document.createElement('button');
+  if (id) btn.id = id;
+  if (className) btn.className = className;
+  btn.innerText = text;
+  btn.type = 'button';
+  return btn;
+}
+
+export function createSnackbar(
+  id,
+  pos = { top: '50%', left: '50%' },
+  textColor = 'white',
+  animationDuration = 3
+) {
+  const posCss = createObjWithTruthyValuesForProps(
+    ['top', 'right', 'bottom', 'left'],
+    pos
+  );
+
+  addStyle(`
+  #${id} {
+    display: none;
+    background-color: #333;
+    color: ${textColor};
+    text-align: center;
+    border-radius: 2px;
+    padding: 16px;
+    position: fixed;
+    z-index: 9999;
+    ${objToCssPropsAndValsStr(posCss)}
+    transform: translate(-50%, -50%);
+  }
+  
+  #${id}.visible {
+    /* https://stackoverflow.com/a/49546937/13727176 */
+    animation-fill-mode: forwards;
+    animation-name: fade-in, fade-out;
+    animation-delay: 0s, ${animationDuration - 0.25}s;
+    animation-duration: 0.25s; /* same for both */
+    display: flex;
+  }
+  
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 0.9;
+    }
+  }
+
+  @keyframes fade-out {
+    to {
+      opacity: 0;
+    }
+  }
+
+  `);
+
+  const snackbar = document.createElement('div');
+  snackbar.id = id;
+
+  return snackbar;
+}
+
+export function showSnackbar(id, text) {
+  const snackbar = document?.getElementById(id);
+  snackbar.remove();
+  document.body.appendChild(snackbar);
+
+  if (snackbar) {
+    snackbar.innerText = text;
+    snackbar.classList.add('visible');
+  } else {
+    console.warn('Snackbar with ID', id, 'not found!');
+  }
+}
+
+// DOM element selection
+
 export function getTextContent(elementSelector, root = document, trim = true) {
   const textContent = selectSingleElement(elementSelector, root)?.textContent;
   if (trim) {
@@ -54,4 +137,31 @@ export function selectElements(
   }
 
   return matches;
+}
+
+// Other
+
+export function addStyle(CSSText) {
+  const style = document.createElement('style');
+  style.appendChild(document.createTextNode(CSSText));
+  document.querySelector('head').appendChild(style);
+}
+
+// Internal
+
+// creates new object from inputObj with only those props that are in the given array of props and have a truthy value in inputObj
+function createObjWithTruthyValuesForProps(props, inputObj) {
+  return props.reduce((prev, propName) => {
+    const propVal = inputObj[propName];
+    if (propVal) {
+      return { ...prev, [propName]: propVal };
+    }
+    return prev;
+  }, {});
+}
+
+function objToCssPropsAndValsStr(obj) {
+  return Object.entries(obj)
+    .map(([prop, val]) => `${prop}: ${val};`)
+    .join('\n');
 }
